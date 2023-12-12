db8d35291b64c893ab339bec7b28f1fa06cf2cb4
Pradeep <pradeep.kumar44@gmail.com>
2023-12-12 03:52:28 +0000

    Update file OrderRestController.java

--- src/main/java/com/classpathio/order/controller/OrderRestController.java
+++ src/main/java/com/classpathio/order/controller/OrderRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.classpathio.order.model.Order;
 import com.classpathio.order.service.OrderService;
-import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor; 
 
 @RestController
 @RequestMapping("/api/v1/orders")
