27a52473ecdec123a00d03e87db4d832b736b0a3
Pradeep <pradeep.kumar44@gmail.com>
2024-05-20 09:46:34 +0000

    Update file StateChangeHandler.java

--- src/main/java/com/classpathio/order/util/StateChangeHandler.java
+++ src/main/java/com/classpathio/order/util/StateChangeHandler.java
@@ -5,8 +5,8 @@ import org.springframework.boot.availability.LivenessState;
 import org.springframework.boot.availability.ReadinessState;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-
 import lombok.extern.slf4j.Slf4j;
+
 
 @Configuration
 @Slf4j
@@ -15,7 +15,5 @@ public class StateChangeHandler {
 	@EventListener(AvailabilityChangeEvent.class)
 	public void livenessStateHandler(AvailabilityChangeEvent<LivenessState> event) {
 		log.info("Liveness:: Application change event :: {}", event.getState());
-		
 	}
-
 }
