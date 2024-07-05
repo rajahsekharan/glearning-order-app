0b60d990eb0b3c45cb43cf648c3ea6f0fdf47451
Pradeep <pradeep.kumar44@gmail.com>
2024-07-05 12:12:55 +0000

    Update file ApplicationConfiguration.java

--- src/main/java/com/classpathio/order/config/ApplicationConfiguration.java
+++ src/main/java/com/classpathio/order/config/ApplicationConfiguration.java
@@ -1,6 +1,5 @@
 package com.classpathio.order.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient; 
